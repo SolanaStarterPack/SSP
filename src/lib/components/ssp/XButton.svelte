dde618660204b2c5ddef8760507e69cb79ccd995
benbrunyee <brunyeeb@gmail.com>
2025-01-06 17:45:31 +0000

    add twitter

--- src/lib/components/ssp/XButton.svelte
+++ src/lib/components/ssp/XButton.svelte
@@ -2,7 +2,7 @@
 	import { Button } from '../ui/button';
 </script>
 
-<Button variant="ssp" class="h-fit !rounded-full !p-2">
+<Button variant="ssp" class="h-fit !rounded-full !p-2" href="https://x.com/tradewithssp">
 	<img src="/logos/ssp-logo-white.png" alt="SSP" class="h-8 w-auto" />
 	<span class="mr-2 text-lg">Follow SSP on X</span>
 </Button>
